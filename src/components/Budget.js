61317d099c629d08f07730b0814885658aced5cc
John Park <park.hokyun@gmail.com>
2023-02-16 00:13:33 -0500

    changed budget to input type

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,9 +2,20 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input
+                                required='required'
+                                type='number'
+                                id='budget'
+                                value={budget}
+                                style={{size: 10}}
+                                onChange={(event) => {budget = event.target.value}}
+                                >
+                </input>
+            </span>
+            
         </div>
     );
 };
